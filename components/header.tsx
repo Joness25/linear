84083d5d48407beb43bb07ca98e0cfb33624a816
Joness25 <jonesngogee@gmail.com>
2024-07-23 23:20:50 +0300

    solving error

--- components/header.tsx
+++ components/header.tsx
@@ -31,7 +31,7 @@ export const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-10 w-full border-b border-transparent-white backdrop-blur-[12px]">
       <Container className="flex h-navigation-height">
-        <Link className="flex items-center text-md" href="/">
+        <Link className="flex justify-between lg:items-center text-md" href="/">
           <Logo className="mr-4 h-[1.8rem] w-[1.8rem]" /> Linear
         </Link>
 
@@ -81,7 +81,7 @@ export const Header = () => {
           </nav>
         </div>
 
-        <div className="hidden lg:flex ml-auto flex h-full items-center">
+        <div className="hidden lg:flex ml-auto  h-full items-center">
           <Link className="mr-6 text-sm" href="#">
             Log in
           </Link>
